3e1cd18fae807310b1e4346fc51e6b98d208799d
KUSH <KUSH42@users.noreply.github.com>
2018-06-16 15:47:25 +0200

    Matrix function working

--- src/app/slaves/slaves.component.ts
+++ src/app/slaves/slaves.component.ts
@@ -16,17 +16,17 @@ export class SlavesComponent implements AfterViewInit {
 
   constructor() { }
 
-  @ViewChild('can') can: ElementRef;
+  @ViewChild('canvas') canvas: ElementRef;
   /** Canvas 2d context */
   private ctx: CanvasRenderingContext2D;
 
   ngAfterViewInit(): void {
 
-    this.ctx = (this.can.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.ctx = (this.canvas.nativeElement as HTMLCanvasElement).getContext('2d');
 
     // making the canvas full screen
-    this.can.nativeElement.style.height = window.innerHeight.toString();
-    this.can.nativeElement.width = window.innerWidth;
+    this.canvas.nativeElement.style.height = window.innerHeight.toString();
+    this.canvas.nativeElement.width = window.innerWidth;
 
     // chinese characters - taken from the unicode charset
     const chinese = '田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑';
@@ -34,7 +34,7 @@ export class SlavesComponent implements AfterViewInit {
     const chineseA = chinese.split('');
 
     const font_size = 10;
-    const columns = this.can.nativeElement.width / font_size; // number of columns for the rain
+    const columns = this.canvas.nativeElement.width / font_size; // number of columns for the rain
     // an array of drops - one per column
     const drops = [];
     // x below is the x coordinate
@@ -43,32 +43,32 @@ export class SlavesComponent implements AfterViewInit {
       drops[x] = 1;
     }
 
-    // drawing the characters
-    function draw() {
-      // Black BG for the canvas
-      // translucent BG to show trail
-      this.ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-      this.ctx.fillRect(0, 0, this.can.nativeElement.width, this.can.nativeElement.height);
 
-      this.ctx.fillStyle = '#0F0'; // green text
-      this.ctx.font = font_size + 'px arial';
-      // looping over drops
-      for (let i = 0; i < drops.length; i++) {
-        // a random chinese character to print
-        const text = chinese[Math.floor(Math.random() * chineseA.length)];
-        // x = i*font_size, y = value of drops[i]*font_size
-        this.ctx.fillText(text, i * font_size, drops[i] * font_size);
-        // sending the drop back to the top randomly after it has crossed the screen
-        // adding a randomness to the reset to make the drops scattered on the Y axis
-        if (drops[i] * font_size > this.can.nativeElement.height && Math.random() > 0.975) {
-          drops[i] = 0;
-        }
-        // incrementing Y coordinate
-        drops[i]++;
+    setInterval(() => { this.draw(font_size, chineseA, drops); }, 33);
+  }
+  // drawing the characters
+  draw(font_size: number, chineseA: string[], drops: number[]) {
+    // Black BG for the canvas
+    // translucent BG to show trail
+    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    this.ctx.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
 
+    this.ctx.fillStyle = '#0F0'; // green text
+    this.ctx.font = font_size + 'px arial';
+    // looping over drops
+    for (let i = 0; i < drops.length; i++) {
+      // a random chinese character to print
+      const text = chineseA[Math.floor(Math.random() * chineseA.length)];
+      // x = i*font_size, y = value of drops[i]*font_size
+      this.ctx.fillText(text, i * font_size, drops[i] * font_size);
+      // sending the drop back to the top randomly after it has crossed the screen
+      // adding a randomness to the reset to make the drops scattered on the Y axis
+      if (drops[i] * font_size > this.canvas.nativeElement.height && Math.random() > 0.975) {
+        drops[i] = 0;
       }
-    }
+      // incrementing Y coordinate
+      drops[i]++;
 
-    setInterval(draw, 33);
+    }
   }
 }
